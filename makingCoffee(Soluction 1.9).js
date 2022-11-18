352da10576a8d467d5021432b13b0c3343a5e698
BiraBalaZ <85596186+BiraBalaZ@users.noreply.github.com>
2022-11-18 20:34:48 -0300

    Versão 1.0.4 - Otimização do Código
    Fazendo com que o subtotal seja calculado automaticamente com um laço de repetição 'for'.

--- makingCoffee(Soluction 1.9).js
+++ makingCoffee(Soluction 1.9).js
@@ -6,105 +6,78 @@ console.log("| |    / _ \\|  _|  _/ _ \\/ _ \\  \\___ \\|  _ \\ / _ \\|  _ \\");
 console.log("| |___| (_) | | | ||  __/  __/  ____) | | | | (_) | |_) |       ");
 console.log("\\______\\___/|_| |_| \\___|\\___| |_____/|_| |_|\\___/| .__/   ");
 console.log("                                                  | |           ");
-console.log("   [Horário de funcionamento: 08h às 18h]         |_|       \n\n");
+console.log("   [Horário de funcionamento: 08h às 22h]         |_|     \n\n\n");
+    
+//Definindo os itens do nosso Menu/Cardápio e seus valores
+var menu = [
+    item1  = {nome:'Café preto           ', valor:'01.00', qnty: 0},
+    item2  = {nome:'Expresso             ', valor:'02.00', qnty: 0},
+    item3  = {nome:'Cappuccino           ', valor:'12.00', qnty: 0},
+    item4  = {nome:'Mocha                ', valor:'10.00', qnty: 0},
+    item5  = {nome:'Mochaccino           ', valor:'15.00', qnty: 0},
+    item6  = {nome:'Machiato             ', valor:'11.50', qnty: 0},
+    item7  = {nome:'Latte                ', valor:'08.00', qnty: 0},
+    item8  = {nome:'Duplo                ', valor:'05.00', qnty: 0},
+    item9  = {nome:'Frappuccino          ', valor:'17.50', qnty: 0},
+    item10 = {nome:'Café com leite      ', valor:'04.00', qnty: 0},
+    item11 = {nome:'Leite               ', valor:'03.50', qnty: 0},
+    item12 = {nome:'X-Burger            ', valor:'25.00', qnty: 0},
+    item13 = {nome:'Donuts              ', valor:'06.00', qnty: 0},
+    item14 = {nome:'Misto quente        ', valor:'10.00', qnty: 0},
+    item15 = {nome:'Brigadeirão         ', valor:'05.00', qnty: 0},
+    item16 = {nome:'Torta de pêssego    ', valor:'07.50', qnty: 0},
+    item17 = {nome:'Torta de maçã       ', valor:'07.50', qnty: 0},
+    item18 = {nome:'Torta de morango    ', valor:'07.50', qnty: 0},
+    item19 = {nome:'Bolo de brigadeiro  ', valor:'04.50', qnty: 0},
+    item20 = {nome:'Bolo de chocolate   ', valor:'04.50', qnty: 0},
+    item21 = {nome:'Bolo de fubá        ', valor:'04.50', qnty: 0},
+    item22 = {nome:'Bolo de morango     ', valor:'04.50', qnty: 0},
+    item23 = {nome:'Pão de queijo (100g)', valor:'02.50', qnty: 0},
+    item23 = {nome:'Brownie             ', valor:'07.00', qnty: 0}
+];
 
-function scr_drinkCoffee() {
-
-    //Variáveis para descobrir a hora do usuário
-    var day  = new Date();
-    var hour = day.getHours();
-
-    //O programa só funciona das 8h às 18h
-    if (hour >= 8 && hour <= 22) {
-
-        console.log('[1] - Expresso     [5] - Duplo\n[2] - Mocha        [6] - Latte\n[3] - Cappuccino   [7] - Café Preto\n[4] - Macchiato    [8] - Mochaccino\n');
-
-        var coffeeDrinked   = 0;
-        var maxCoffees      = 10;
-        var menu            = ['Expresso', 'Mocha', 'Cappuccino', 'Macchiato', 'Duplo', 'Latte', 'Café Preto', 'Mochaccino'];
-        var product         = prompt('Qual Café vai querer? ');
-        var coffeeSelected  = product-1;
-
-        function scr_loop() {
-            for (var refil = coffeesToDrink; refil > 0; refil --) {
-                        
-                //Adicionando mais um café pra conta
-                coffeeDrinked += 1;
-                
-                //Dizendo em qual café estou
-                console.log(`Bebendo o ${coffeeDrinked}º ${menu[coffeeSelected]}`);
-        
-                console.log('    (   )  (   )  )       ');
-                console.log('     ) (    ) (  (        ');
-                console.log('    (   )  (   )  )       ');
-                console.log('     _____________        ');
-                console.log('    <_____________> ___   ');
-                console.log('    |             |/ _ \\ ');
-                console.log('    |             | | | | ');
-                console.log(`    |      ${coffeeDrinked}      | |_| | `);
-                console.log(' ___|             |\\___/ ');
-                console.log('/    \\___________/    \\ ');
-                console.log('\\_____________________/  \n');
-        
-                if (refil != 1) {
-                    console.log('Pegando mais café\n');
-                }
-                else if (refil == 1) { //Se o refil chegar a um, ele escreve que acabou o café
-                    console.log(`O ${menu[coffeeSelected]} acabou.\n`);
-                    console.log(`Preparando mais uma rodada de café.\n`);
-        
-                    //Chamando a função novamente para beber café para sempreee
-                    scr_drinkCoffee();
-                }
-            }
-        }
-
-        //Definindo o primeiro e o ultimo item do nosso vetor
-        var primVetor = menu.length - (menu.length)
-        var ultmVetor = primVetor   + (menu.length)
-
-        //Se for menor que o primeiro item ou maior que o ultimo item, ele exibe a mensagem:
-        if (coffeeSelected < primVetor || coffeeSelected >= ultmVetor) {
-            console.log('Ops, não temos esse item no nosso cardápio!');
-        }
-        else {
-            var coffeesToDrink = prompt(`Digite aqui a quantidade de xícaras de ${menu[coffeeSelected]} (de 1 a ${maxCoffees}) que deseja beber `);
-
-            //Se a quantidade de cafés for maios que a quantidade máxima, 
-            //a seguinte mensagem será exibida e será preparada a quantidade maxima de cafés.
-            if (coffeesToDrink > maxCoffees) {
-                console.log(`\n[ATENÇÃO] Não temos tudo isso de ${menu[coffeeSelected]}! Vamos preparar ${maxCoffees} xícaras para beber.`);
-                coffeesToDrink = maxCoffees;
-                scr_loop();
-            }
-            else if (coffeesToDrink == 1) {
-                console.log(`Saindo ${coffeesToDrink} xícara de ${menu[coffeeSelected]}.\n`);
-                scr_loop();
-            }
-            else if (coffeesToDrink > 1){
-                console.log(`Saindo ${coffeesToDrink} xícaras de ${menu[coffeeSelected]}.\n`);
-                scr_loop();
-            }
-            else if (coffeesToDrink <= 0) {
-                console.log('Impossível calcular o numero de xícaras!');
-                coffeesToDrink = prompt(`Digite aqui a quantidade de xícaras de ${menu[coffeeSelected]} (de 1 a ${maxCoffees}) que deseja beber `);
-            }
-        }
+//#region: Imprimindo o cardápio
+    console.log('                        Dê uma olhada no nosso carápio!\n');
+    j=1
+    for (i = 0; i < menu.length; i += 2) {
+        console.log(`[${i+1}] ${menu[i].nome} | R$ ${menu[i].valor}   -   [${j+1}] ${menu[j].nome} | R$ ${menu[j].valor}`);
+        j+=2;
     }
-    else {
-        console.log('Poxa que azar! Fechamos, que tal voltar amanhã?');
-    }
+//#endregion
+
+//Função de anotar o pedido
+function anotarPedido() {
+
+    //Pedindo o codigo do item
+    anotar = prompt('\nDigite o valor do item que deseja: ');
+    numPedido = anotar-1;
+
+    //Mostar item selecionado
+    console.log(`Selecionado: ${menu[numPedido].nome}`);
+
+    //Quantidade de itens que o usuário deseja
+    quantidade = prompt('Quantos vai querer? ');
+
+    //Aumentando a quantidade do item selecionado
+    menu[numPedido].qnty += quantidade;
+
+    //Perguntando se o cliente quer mais alguma coisa
+    resposta = prompt('Deseja pedir mais alguma coisa? ');
+
+    //Transformando a resposta em minuscula para ter apenas uma verificação
+    resp = resposta.toLowerCase();
 }
 
-//Rodando meu Script pela primeira vez!
-scr_drinkCoffee();
+//Anotando o pedido pela primeira vez
+anotarPedido();
 
-                                  /*
- ____ _____ _____               / /  ____          _                ______
-|  _ \_   _|  __ \     /\      / /  |  _ \   /\   | |        /\    |___  /   
-| |_) || | | |__) |   /  \    / /__ | |_) | /  \  | |       /  \      / /    
-|  _ < | | |  _  /   / /\ \  |___  /|  _ < / /\ \ | |      / /\ \    / /     
-| |_) || |_| | \ \  / ____ \    / / | |_) / ____ \| |____ / ____ \  / /__    
-|____/_____|_|  \_\/_/    \_\  / /  \____/_/    \_\______/_/    \_\/_____|   
-                              / /     
-                             */
+//Enquanto a resposta for sim ele segue anotando o pedido
+while (resp == 'sim') { anotarPedido() }
+
+//Subtotal da compra, como os itens que nao foram selecionados se multiplicam por ZERO e viram ZERO, não altera o valor total REAL
+//Pegando o subtotal ser somado automaticamente sem inclusão das variáveis
+subtotal = 0;
+for (k = 0; k < menu.length; k += 1) { subtotal += menu[k].valor * menu[k].qnty; }
+
+//Multiplicando o valor do item pela quantidade solicitada
+console.log(`SubTotal: R$ ${subtotal}`);
